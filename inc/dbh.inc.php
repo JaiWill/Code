716a0e2465805a0f04abc81e7ad47a017b5f6659
JaiWill <86173929+JaiWill@users.noreply.github.com>
2021-06-19 21:20:32 -0500

    Connection Info

--- inc/dbh.inc.php
+++ inc/dbh.inc.php
@@ -1,8 +1,16 @@
 <?php
+	
+	//Developoment Connection
+	//$dbServername = "localhost";
+	//$dbUsername = "root";
+	//$dbPassword = "";
+	//$dbName = "nova";
+	
 	$dbServername = "localhost";
 	$dbUsername = "root";
 	$dbPassword = "";
 	$dbName = "nova";
+	
 	
 	$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
 	
--- inc/dbho.inc.php
+++ inc/dbho.inc.php
@@ -1,8 +1,18 @@
 <?php
-	$dbServername = "localhost";
-	$dbUsername = "root";
-	$dbPassword = "";
-	$dbName = "nova";
+	
+	//Developoment Connection
+	//$dbServername = "localhost";
+	//$dbUsername = "root";
+	//$dbPassword = "";
+	//$dbName = "nova";
+	
+	
+	//Remote Connection
+	$dbServername = "remotemysql.com";
+	$dbUsername = "2AI812dNkZ";
+	$dbPassword = "twrkg81IYR";
+	$dbName = "2AI812dNkZ";
+	
 	
 	$conn = new PDO("mysql: host=$dbServername; dbname=$dbName","$dbUsername", "$dbPassword");
 	//$connect = new PDO("mysql:host=localhost;dbname=testing4", "root", "");
