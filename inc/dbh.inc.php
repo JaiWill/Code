3f6867c6925df7cf787ebd8aa2d6577d8deaa49d
JaiWill <86173929+JaiWill@users.noreply.github.com>
2021-06-19 21:47:28 -0500

    Connection settings

--- inc/dbh.inc.php
+++ inc/dbh.inc.php
@@ -6,10 +6,11 @@
 	//$dbPassword = "";
 	//$dbName = "nova";
 	
-	$dbServername = "localhost";
-	$dbUsername = "root";
-	$dbPassword = "";
-	$dbName = "nova";
+	//Remote Connection
+	$dbServername = "remotemysql.com";
+	$dbUsername = "2AI812dNkZ";
+	$dbPassword = "twrkg81IYR";
+	$dbName = "2AI812dNkZ";
 	
 	
 	$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
